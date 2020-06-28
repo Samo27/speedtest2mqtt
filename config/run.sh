3b47c3a4e616764508eb24b646c837c62a0a043a
Samo27 <samo.kramberger.27@gmail.com>
2020-06-28 23:14:45 +0200

    Add files via upload

--- config/run.sh
+++ config/run.sh
@@ -3,12 +3,19 @@ echo "Starting run.sh"
 
 cat /var/speedtest/config/crontab.default > /var/speedtest/config/crontab
 
-if [[ ${CRONJOB_ITERATION} && ${CRONJOB_ITERATION-x} ]]; then
-    sed -i -e "s/0/*\/${CRONJOB_ITERATION}/g" /var/speedtest/config/crontab
+if [[ -z ${CRONJOB_ITERATION} ]]; then
+    CRONJOB_ITERATION_SCRIPT="15"
+else
+    CRONJOB_ITERATION_SCRIPT=${CRONJOB_ITERATION}
 fi
+
+#if [[ ${CRONJOB_ITERATION} ]]; then
+    sed -i -e "s/0/*\/$CRONJOB_ITERATION_SCRIPT/g" /var/speedtest/config/crontab
+    #sed  '0,/\[0\]/i $CRONJOB_ITERATION_SCRIPT' /var/speedtest/config/crontab
+#fi
 crontab /var/speedtest/config/crontab
 
 echo "Starting Cronjob"
-crond -l 2 -f &
+cron -l 2 -f &
 
 exit 0;
